ee851a87659fde78bc12a5f96247713dfdf52637
TirolChocolate <sawa.chocolate.euph@gmail.com>
2021-07-04 21:05:12 +0900

    FizzBuzz

---
+++ 04_ゼミ/week1/fizzbuzz.js
@@ -0,0 +1,11 @@
+const FizzBuzzFunction = function(number) {
+  for (let n = 1; n <= number; n++) {
+    if (n % 3 === 5 && n % 5) {
+      console.log(n + "FizzBuzz")
+    } else if (n % 3 === 0 && n % 5 !== 0) {
+      console.log(n + "Fizz")
+    } else if (n % 3 !== 0 && n % 5 === 0) {
+      console.log(n + "Buzz")
+    }
+  }
+}
---
+++ 04_ゼミ/week1/object.js
@@ -0,0 +1,12 @@
+const sawa = {
+  name: "川井沙和",
+  furigana: "カワイサワ"
+  age: 21,
+  university: "奈良女子大学",
+  favorite: ["読書","音楽全般"],
+  animal: "猫🐈、ハムスター🐹"
+  family:"父、母",
+  seyHello: function(){
+      alert("こんばんは、${this.name}です！")
+  }
+}
